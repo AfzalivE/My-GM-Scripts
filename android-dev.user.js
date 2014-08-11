437aba778c7319f1b15630f245e5f8c2a354f511
Afzal Najam <afzalnaj@gmail.com>
2014-08-10 21:43:03 -0400

    Updated and added a few scripts

--- android-dev.user.js
+++ android-dev.user.js
@@ -1,61 +1,88 @@
 // ==UserScript==
-// @name          Android Market Developer
-// @description	  Adds some links helpful to developers in the Android Market
+// @name          Android Developer Console
+// @description   Adds some links helpful to developers in the Android Market
 // @include       https://play.google.com/*
-// @version       4.2
+// @match         https://play.google.com/*
+// @version       5.1
 // @author        Afzal Najam
-// @changelog     Fixed after the Developer Console team update
+// @changelog     Modified for new Play Store
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// @grant         GM_setValue
+// @grant         GM_getValue
+// @grant         GM_listValues
+// @grant         GM_deleteValue
+// @grant         GM_log
 // ==/UserScript==
 
-dropdown = document.getElementById('gbom');
-if (dropdown) {
-        listitem = document.createElement('li');
-        listitem.className = 'gbe gbmtc';
-        devlink = document.createElement('a');
-        devlink.className = 'gbmt';
-        devlink.setAttribute('href','https://play.google.com/apps/publish/Home');
-        devlink.setAttribute('target','_blank');
-        devlink.innerHTML = 'My Developer Console';
+myOrders = $('li[jsinstance ^="0"]');
+if (myOrders) {
+    listitem = document.createElement('div');
+    listitem.className = 'leaf-submenu-link-wrapper';
+    devlink = document.createElement('a');
+    devlink.className = 'leaf-submenu-link';
+    devlink.setAttribute('href', 'https://play.google.com/apps/publish/');
+    devlink.setAttribute('target', '_blank');
+    devlink.innerHTML = 'My developer console';
 
-        listitem.appendChild(devlink);
-        dropdown.appendChild(listitem);
+    listitem.appendChild(devlink);
+    myOrders.after(listitem);
 }
 
-var patt = /^https:\/\/play.google.com\/apps\/publish\/Home\?dev_acc=\d+[#]*$/i;
-if (patt.test(document.location.href)) {
-    var _timer = setInterval(isPageReady, 500); 
+if (window.location.hash.indexOf("AppListPlace") > 0) {
+    archiveItems();
 }
 
-function isPageReady() {
-    if (document.getElementById('la-loadingBox').style.display == 'none') {
-        function getElementsById(id) {
-           var a = document.getElementsByTagName("a");
-           var arr = new Array();
+function archiveItems() {
+    updateP = $("p[data-column*='UPDATE']");
 
-           for (var i=0; i < a.length; i++) {
-              if (a[i].id == id) {
-                 arr[arr.length] = a[i];
-              }
-           }
-           return arr;
-        }
-        srclinks = getElementsById('gwt-debug-statsLink');
-        for (i = 0; i < srclinks.length; i++) {
-            clearInterval(_timer);
-            var statlink = srclinks[i].href;
-            var patt = /StatsPlace.p=(.+)/i;
-            var pkg = statlink.match(patt)[1];
-            var link = "https://play.google.com/store/apps/details?id=" + pkg;
-            var a = document.createElement("a");
-            a.href = link;
-            a.innerHTML = "Market Link";
-            tr = document.createElement("tr");
-            listdiv = document.getElementById('gwt-debug-applistingappList');
-            targettd = listdiv.childNodes[i].getElementsByTagName("div")[0].getElementsByTagName("table")[0].firstChild;
-            listdiv.childNodes[i].getElementsByTagName("div")[0].getElementsByTagName("table")[0].firstChild;
-            tr.appendChild(a);
-            targettd.appendChild(tr);
-        }
-        return;
+    if (updateP) {
+        var archiveLink = $('<a>', {
+            href: "#test",
+            style: "background: no-repeat url(//ssl.gstatic.com/mail/sprites/general_black-16bf964ab5b51c4b7462e4429bfa7fe8.png) 0 -17px; width: 25px; height: 25px; display: inline-block; vertical-align: -50%;",
+            onclick: "GM_setValue('hiddenApps', 'testing')",
+        });
+        updateP.prepend(archiveLink);
+    } else {
+        console.log("updateP not found");
+    }
+    var vals = [];
+    var values = GM_listValues();
+    for (var i = 0; i < values.length; i++) {
+        vals.push(GM_getValue(GM_listValues()[i]));
+    }
+    GM_log(vals);
+}
+
+function resetArchived() {
+    GM_deleteValue("hiddenApps");
+}
+
+function setArchived(appName) {
+    GM_setValue("hiddenApps", appName);
+}
+
+function showStoreLink() {
+    pubButton = $("button>*:contains('Published')");
+    if (pubButton.length > 0) {
+        appName = window.location.hash.split("=")[1];
+        element = pubButton.parent().parent().parent().next().next();
+        link = "https://play.google.com/store/apps/details?id=" + appName;
+        element.html("- ");
+        $('<a>', {
+            text: appName,
+            href: link,
+        }).appendTo(element);
     }
 }
+
+function locationHashChanged() {
+    locationHash = window.location.hash;
+    if (locationHash.indexOf("AppListPlace") > 0) {
+        console.log("archive icons");
+        archiveItems();
+    } else if (locationHash.indexOf(":p=") > 0) {
+        showStoreLink();
+    }
+}
+
+window.addEventListener("hashchange", locationHashChanged);
